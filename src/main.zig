eeec61cc96873744437ef76a52cbf8371594b1cc
Aaron Winter <winter.aaron93@gmail.com>
2022-12-25 12:37:04 +0100

    moving sprites

--- src/main.zig
+++ src/main.zig
@@ -22,6 +22,7 @@ pub fn main() !void {
     try setup(&world, texture);
 
     while (!rl.WindowShouldClose()) {
+        try moveSystem(&world);
         try renderSystem(&world);
     }
 
@@ -33,19 +34,57 @@ const Position = struct {
     y: f32,
 };
 
+const Velocity = struct {
+    x: f32,
+    y: f32,
+};
+
 const Sprite = struct {
     texture: rl.Texture2D,
 };
 
 fn setup(world: *World, texture: rl.Texture2D) !void {
+    var rnd = std.rand.DefaultPrng.init(0);
+
     var y: u32 = 0;
     while (y < 10) : (y += 1) {
         var x: u32 = 0;
         while (x < 10) : (x += 1) {
             var e = try world.entities.spawn();
+            var rx = rnd.random().int(i32);
+            var ry = rnd.random().int(i32);
+
+            var dx = @rem(rx, 10) - @as(i32, 5);
+            var dy = @rem(ry, 10) - @as(i32, 5);
+
             try world.entities.set(Position, e, .{ .x = @intToFloat(f32, x * 64), .y = @intToFloat(f32, y * 64) });
+            try world.entities.set(Velocity, e, .{ .x = @intToFloat(f32, dx), .y = @intToFloat(f32, dy) });
             try world.entities.set(Sprite, e, .{ .texture = texture });
         }
+    }
+}
+
+fn moveSystem(world: *World) !void {
+    var query = try world.query(.{ .pos = Position, .vel = Velocity });
+    var it = query.iter();
+
+    const width = @intToFloat(f32, rl.GetRenderWidth());
+    const height = @intToFloat(f32, rl.GetRenderHeight());
+
+    while (it.next()) |e| {
+        e.pos.x += e.vel.x;
+        e.pos.y += e.vel.y;
+
+        if (e.pos.x < 0 or e.pos.x > width) {
+            e.vel.x = -e.vel.x;
+        }
+
+        if (e.pos.y < 0 or e.pos.y > height) {
+            e.vel.y = -e.vel.y;
+        }
+
+        e.pos.x = std.math.clamp(e.pos.x, 0, width);
+        e.pos.y = std.math.clamp(e.pos.y, 0, height);
     }
 }
 
